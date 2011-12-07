d01fbce939ddc17c565a934968e025c11637f580
Gonzalo Correa <gcorrea.q@gmail.com>
2011-12-07 01:17:50 -0300

    Agregado ay2_ej2 sobre saludos dependiendo de la hora del día

---
+++ ayudantia_1/ay1_ej2.py
@@ -0,0 +1,12 @@
+hour = int(raw_input('Hora:'))
+meridiem = str(raw_input('AM o PM?: '))
+
+if meridiem == 'am':
+	print 'Buenos dias!'
+elif meridiem == 'pm':
+	if 8 <= hour <= 11:
+		print 'Buenas noches!'
+	else:
+		print 'Buenas tardes!'
+else:
+	print 'Error!'
