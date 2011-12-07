d96d59427f05aeafd4d53c0b3f9b971c781a7061
Gonzalo Correa <gcorrea.q@gmail.com>
2011-12-07 01:56:25 -0300

    Agregado ay1_ej3 con calculadora

---
+++ ayudantia_1/ay1_ej3.py
@@ -0,0 +1,18 @@
+operand_1 = float(raw_input('Operando: ')) 
+operator = str(raw_input('Operador: '))
+operand_2 = float(raw_input('Operando: '))
+
+if operator == '+':
+	result = operand_1 + operand_2
+elif operator == '-':
+	result = operand_1 - operand_2
+elif operator == '*':
+	result = operand_1 * operand_2
+elif operator == '/':
+	result = operand_1 / operand_2
+elif operator == '**':
+	result = operand_1 ** operand_2
+else:
+	print 'Error! Operando incorrecto!'
+
+print operand_1, operator, operand_2, '=', result
