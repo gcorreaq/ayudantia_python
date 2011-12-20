611754b8188780c6a27afac384fcc5a5819987c5
Gonzalo Correa <gcorrea.q@gmail.com>
2011-12-19 22:05:52 -0300

    Agregada solución más simple del ejercicio de la torre y el alfil

---
+++ ayudantia_3/ay3_ej1_simple.py
@@ -0,0 +1,11 @@
+bishop_row = int(raw_input("Alfil, Fila: "))
+bishop_col = int(raw_input("Alfil, Columna: "))
+rook_row = int(raw_input("Torre, Fila: "))
+rook_col = int(raw_input("Torre, Columna: "))
+
+if rook_col == bishop_col or rook_row == bishop_row:
+	print 'Torre captura alfil'
+elif abs(bishop_row - rook_row) == abs(bishop_col - rook_col):
+	print 'Alfil captura torre'
+else:
+	print 'Ups!'
