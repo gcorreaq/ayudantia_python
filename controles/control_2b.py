5ecb68fea1d4a55ff88fe5045a8abf2ce8393c57
Gonzalo Correa <gcorrea.q@gmail.com>
2012-01-14 03:54:29 -0300

    Agregada solución de algunos controles on-line

---
+++ controles/control_2b.py
@@ -0,0 +1,14 @@
+# Control 2B
+
+monto_inicial = int(raw_input("Monto inicial: "))
+monto = monto_inicial
+
+for i in range(1,4):
+
+	prediccion = int(raw_input("Prediccion dia " + str(i) + " (en %): "))
+	monto += ((monto * prediccion) / 100)
+
+	if monto >= monto_inicial:
+		print 'Hay ganancia de dinero'
+	else:
+		print 'Hay perdida de dinero'
---
+++ controles/control_3.py
@@ -0,0 +1,39 @@
+# Control 3
+
+success = 0
+
+for i in range(3):
+	# Este valor es arbitrario, y solo para poder entrar a los 'while'
+	num1 = 0
+	num2 = 0
+
+	# Se pedira el numero hasta que cumpla la condicion
+	while not (10 < num1 < 100):
+		num1 = int(raw_input("Numero 1: "))
+
+		# Mensaje de error en caso de que numero sea invalido
+		if not (10 < num1 < 100):
+			print 'Numero invalido!'
+
+	# Se pedira el numero hasta que cumpla la condicion
+	while not (10 < num2 < 100):
+		num2 = int(raw_input("Numero 2: "))
+
+		# Mensaje de error en caso de que numero sea invalido
+		if not (10 < num2 < 100):
+			print 'Numero invalido!'
+
+	# Si al calcular modulo 2 los resultados son distintos,
+	# entonces uno es par y el otro impar
+	if (num1 % 2) != (num2 % 2):
+		print 'Exitoso'
+		success += 1
+	else:
+		print 'No exitoso'
+
+print 'Usted ha',
+
+if success == 2 or success == 3:
+	print 'ganado'
+else:
+	print 'perdido'
