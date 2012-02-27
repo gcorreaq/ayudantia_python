13abc1e8cf291ad92f60e54116a2a313e23f18e4
Gonzalo Correa <gcorrea.q@gmail.com>
2012-02-27 01:21:26 -0300

    Agregadas preguntas del certamen 2 tipo. Falta el código de pruebas y terminar la pregunta 4

---
+++ certamenes/c2_tipo/pregunta_3.py
@@ -0,0 +1,41 @@
+def cuantos_escucharon(c):
+	audiencia = 0
+
+	for concierto in conciertos:
+		if c in concierto['canciones']:
+			audiencia += concierto['publico']
+
+	return audiencia
+
+def mismo_concierto(c1, c2):
+	for concierto in conciertos:
+		if (c1 in concierto['canciones']) and (c2 in concierto['canciones']):
+			return True
+
+	return False
+
+####################
+# Código de prueba #
+####################
+
+conciertos = [
+	{
+		'ciudad': 'Cairo',
+		'publico': 30000,
+		'canciones': [
+			'If your name is main',
+			'Break my heart and return',
+			'Strings float around you',
+		]
+	},
+	{
+		'ciudad': 'Moscu',
+		'publico': 25000,
+		'canciones': [
+			'Break my heart and return',
+			'Open your mind, close your file',
+			'Value error'
+		]
+	}
+]
+
---
+++ certamenes/c2_tipo/pregunta_4.py
@@ -0,0 +1,17 @@
+def canciones_pais(p):
+	canciones = set()
+
+	for pais in ciudades[p]:
+		for concierto in conciertos:
+			if concierto['ciudad'] in pais:
+				canciones |= set(concierto['canciones'])
+
+	# Esto dice: retorna 'canciones' si 'canciones' tiene elementos,
+	# si no, retorna None
+	return canciones if len(canciones) > 0 else None
+
+def contar_exitos(n):
+
+	for pais in ciudades:
+		if len(pais) > 1:
+			
