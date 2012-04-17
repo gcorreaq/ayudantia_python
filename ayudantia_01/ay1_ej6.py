0bd80e014325871b7b251234cd8b16dcd255fa58
Gonzalo Correa <gcorrea.q@gmail.com>
2012-04-16 22:23:21 -0300

    Agregado ejercicio de ordenamiento con estructuras condicionales

--- ayudantia_01/ay1_ej6.py
+++ ayudantia_01/ay1_ej6.py
@@ -2,16 +2,30 @@ num_1 = int(raw_input('Num 1: '))
 num_2 = int(raw_input('Num 2: '))
 num_3 = int(raw_input('Num 3: '))
 
-#if num_1 < num_2 < num_3:
-#	print num_1, num_2, num_3
-#elif num_1 > num_2 > num_3:
-#	print num_3, num_2, num_1
-
 if num_1 < num_2:
 	if num_1 < num_3:
+		# ya sabemos que num_1 es el menor
+		print num_1,
+
 		if num_2 < num_3:
-			print num_1, num_2, num_3
+			print num_2, num_3
 		else:
-			print num_1, num_3, num_2
+			print num_3, num_2
 	elif num_3 < num_2:
-		print num_3, num_1, num_2
+		print num_3, num_1, num_2 
+elif num_2 < num_3:
+	# num_2 es el menor de todos
+	print num_2,
+
+	if num_3 < num_1:
+		print num_3, num_1
+	else:
+		print num_1, num_3
+elif num_3 < num_1:
+	# num_2 es el menor
+	print num_2,
+
+	if num_3 < num_1:
+		print num_3, num_1
+	else:
+		print num_1, num_3
