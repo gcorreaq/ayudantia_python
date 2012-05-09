e38dffda43f90e3ac69474a49f3f3fbb0aabc820
Gonzalo Correa <gcorrea.q@gmail.com>
2012-05-09 03:00:21 -0400

    Agregada solución "piezas de domino" (http://progra.usm.cl/apunte/ejercicios/1/piezas-domino.html)

---
+++ varios/piezas-domino.py
@@ -0,0 +1,17 @@
+suma = 0
+contador = 0
+
+# Se llega hasta 7 para que el ultimo
+# numero de la serie sea un 6
+for lado_1 in range(7):
+    for lado_2 in range(lado_1, 7):
+
+        # Para mostrar la 'pieza' actual
+        print "{0} | {1}".format(lado_1, lado_2)
+        # Se suman los valores
+        suma += (lado_1 + lado_2)
+
+        contador += 1
+
+print "Total:", suma
+print "Cantidad de piezas:", contador
